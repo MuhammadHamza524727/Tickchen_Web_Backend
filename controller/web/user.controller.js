2a1b86c2966bb8573c609e44c7e88d5c024a1e2a
Muhammad Hamza <MuhammadHamza524727@users.noreply.github.com>
2025-08-20 11:34:52 +0500

    login auth updated

--- controller/web/user.controller.js
+++ controller/web/user.controller.js
@@ -322,7 +322,7 @@ exports.login = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: true,
+      secure: process.env.NODE_ENV === "production",
       sameSite: "None",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
